a3f32f990f494104147c52c5ecbe2bda676504ca
Christopher Schultz <chris@christopherschultz.net>
2026-08-15 08:19:49 -0400

    Use better detaults for code formatting
    (In my opinion)

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -16,61 +16,61 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
- * Represents the Fishsticks intake. This is only the actual intake part
- * of the robot, and not the extension, which can be found elsewhere.
- * 
+ * Represents the Fishsticks intake. This is only the actual intake part of the
+ * robot, and not the extension, which can be found elsewhere.
+ *
  * @see IntakeExtension
  */
 @Logged
 public class Intake extends SubsystemBase {
-  public static final AngularVelocity TARGET_SPEED = Units.RPM.of(2000);
-  public static final AngularVelocity IDLE_SPEED = Units.RPM.of(0);
-  public static final int CURRENT_LIMIT = (int) Units.Amps.of(20).in(Amps);
-  public static final int MOTOR_ID = 0;
+	public static final AngularVelocity TARGET_SPEED = Units.RPM.of(2000);
+	public static final AngularVelocity IDLE_SPEED = Units.RPM.of(0);
+	public static final int CURRENT_LIMIT = (int) Units.Amps.of(20).in(Amps);
+	public static final int MOTOR_ID = 0;
 
-  private final SparkMax motor;
-  private final SparkMaxConfig config;
+	private final SparkMax motor;
+	private final SparkMaxConfig config;
 
-  // Stuff for logging
-  double setpoint; // Requested velocity target
-  double current; // Motor current draw
-  double voltage; // Motor voltage applied
-  double velocity; // Motor actual velocity
+	// Stuff for logging
+	double setpoint; // Requested velocity target
+	double current; // Motor current draw
+	double voltage; // Motor voltage applied
+	double velocity; // Motor actual velocity
 
-  public Intake(boolean isRealRobot) {
-    motor = new SparkMax(MOTOR_ID, MotorType.kBrushless);
-    config = new SparkMaxConfig();
-    config.inverted(false);
-    config.smartCurrentLimit(CURRENT_LIMIT);
-    motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+	public Intake(boolean isRealRobot) {
+		motor = new SparkMax(MOTOR_ID, MotorType.kBrushless);
+		config = new SparkMaxConfig();
+		config.inverted(false);
+		config.smartCurrentLimit(CURRENT_LIMIT);
+		motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+	}
 
-  private void setSpeed(AngularVelocity speed) {
-    motor.set(setpoint = speed.in(RPM));
-  }
+	private void setSpeed(AngularVelocity speed) {
+		motor.set(setpoint = speed.in(RPM));
+	}
 
-  public Command setSpeedCommand(AngularVelocity speed) {
-    return Commands.runOnce(() -> this.setSpeed(speed), this);
-  }
+	public Command setSpeedCommand(AngularVelocity speed) {
+		return Commands.runOnce(() -> this.setSpeed(speed), this);
+	}
 
-  public Command runIntake() {
-    return setSpeedCommand(TARGET_SPEED);
-  }
+	public Command runIntake() {
+		return setSpeedCommand(TARGET_SPEED);
+	}
 
-  public Command idleIntake() {
-    return setSpeedCommand(IDLE_SPEED);
-  }
+	public Command idleIntake() {
+		return setSpeedCommand(IDLE_SPEED);
+	}
 
-  @Override
-  public void periodic() {
-    current = motor.getOutputCurrent();
-    voltage = motor.getAppliedOutput() * motor.getBusVoltage();
-    velocity = motor.getEncoder().getVelocity();
-  }
+	@Override
+	public void periodic() {
+		current = motor.getOutputCurrent();
+		voltage = motor.getAppliedOutput() * motor.getBusVoltage();
+		velocity = motor.getEncoder().getVelocity();
+	}
 
-  @Override
-  public void simulationPeriodic() {
-    // Pretend that everything happens instantaneously
-    velocity = setpoint;
-  }
+	@Override
+	public void simulationPeriodic() {
+		// Pretend that everything happens instantaneously
+		velocity = setpoint;
+	}
 }
