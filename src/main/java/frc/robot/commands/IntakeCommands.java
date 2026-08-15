dcb4e04061c3e777f25888d1a72afc9ce535f1a3
Christopher Schultz <chris@christopherschultz.net>
2026-08-15 12:11:12 -0400

    Add a note about stop conditions

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -6,10 +6,16 @@ import frc.robot.subsystems.IntakeExtension;
 
 public class IntakeCommands {
     public static Command getRunIntakeCommand(IntakeExtension intakeExtension, Intake intake) {
+        // NOTE: The forward-limit will automatically stop the motor,
+        // so there is no reason to specifically program a stop-condition
+        // into this command.
         return intakeExtension.deployExtension().until(intakeExtension::getForwardLimit).andThen(intake.runIntake());
     }
 
     public static Command getStopIntakeCommand(IntakeExtension intakeExtension, Intake intake) {
+        // NOTE: The forward-limit will automatically stop the motor,
+        // so there is no reason to specifically program a stop-condition
+        // into this command.
         return intakeExtension.retractExtension().until(intakeExtension::getReverseLimit).andThen(intake.idleIntake());
     };
 }
