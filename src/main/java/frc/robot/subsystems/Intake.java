d20f28f856952328f77a5d472db9e280e243c843
Christopher Schultz <chris@christopherschultz.net>
2026-08-15 11:51:54 -0400

    Use spinning subsystem base class

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,19 +1,9 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Amps;
-import static edu.wpi.first.units.Units.RPM;
-
-import com.revrobotics.PersistMode;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
  * Represents the Fishsticks intake. This is only the actual intake part of the robot, and not the
@@ -22,55 +12,20 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * @see IntakeExtension
  */
 @Logged
-public class Intake extends SubsystemBase {
+public class Intake extends SimpleSpinningSubsystem {
     public static final AngularVelocity TARGET_SPEED = Units.RPM.of(2000);
-    public static final AngularVelocity IDLE_SPEED = Units.RPM.of(0);
-    public static final int CURRENT_LIMIT = (int) Units.Amps.of(20).in(Amps);
+
     public static final int MOTOR_ID = 0;
 
-    private final SparkMax motor;
-    private final SparkMaxConfig config;
-
-    // Stuff for logging
-    double setpoint; // Requested velocity target
-    double current; // Motor current draw
-    double voltage; // Motor voltage applied
-    double velocity; // Motor actual velocity
-
     public Intake(boolean isRealRobot) {
-        motor = new SparkMax(MOTOR_ID, MotorType.kBrushless);
-        config = new SparkMaxConfig();
-        config.inverted(false);
-        config.smartCurrentLimit(CURRENT_LIMIT);
-        motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
-
-    private void setSpeed(AngularVelocity speed) {
-        motor.set(setpoint = speed.in(RPM));
-    }
-
-    public Command setSpeedCommand(AngularVelocity speed) {
-        return Commands.runOnce(() -> this.setSpeed(speed), this);
+        super(isRealRobot, MOTOR_ID, DEFAULT_CURRENT_LIMIT, NEO_MAX, DEFAULT_MAX_ACCELERATION);
     }
 
     public Command runIntake() {
-        return setSpeedCommand(TARGET_SPEED);
+        return setTargetVelocityCommand(TARGET_SPEED);
     }
 
     public Command idleIntake() {
-        return setSpeedCommand(IDLE_SPEED);
-    }
-
-    @Override
-    public void periodic() {
-        current = motor.getOutputCurrent();
-        voltage = motor.getAppliedOutput() * motor.getBusVoltage();
-        velocity = motor.getEncoder().getVelocity();
-    }
-
-    @Override
-    public void simulationPeriodic() {
-        // Pretend that everything happens instantaneously
-        velocity = setpoint;
+        return setTargetVelocityCommand(ZERO_VELOCITY);
     }
 }
